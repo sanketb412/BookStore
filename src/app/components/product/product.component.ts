6ab231b28a2e36a1abf1ecba4c80447d14631e77
sanketb412 <sanketbagde38@gmail.com>
2021-08-03 21:04:00 +0530

    Refactor: code

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,3 +1,4 @@
+import { getNumberOfCurrencyDigits } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BookServiceService } from 'src/app/services/bookService/book-service.service';
@@ -19,39 +20,68 @@ export class ProductComponent implements OnInit {
   cart1:any
   cart: any
   token: any
+  bookId: any
+  homeBook: any
+  cartBook: any
+
   constructor( private router: Router, private service: BookServiceService,  private route: ActivatedRoute, private sibService: SiblingService) { 
   }
 
   ngOnInit(): void {
     this.token = localStorage.getItem('token')    
     // console.log(this.token);
+    this.getCount()
+
+    for(this.homeBook of this.books){
+      console.log("homeId", this.homeBook)
+    
+      // for(this.cartBook of this.cartBooks){
+      //   let cartId = console.log("cartId ", this.cartBook._id)
+    
+      //   if(cartId==homeId){
+      //     this.cart.displaybag = true;
+      //   }
+      // }
+    
+  }
+}
+
+  getCount(){
     this.service.getCart(this.token).subscribe((data:any)=>{
-      this.cartBooks=data['result']  
-      
+      this.cartBooks=data['result']
+  
       this.sibService.communicateMessage(this.cartBooks.length) 
     })
+
+    for(this.homeBook of this.books){
+      console.log("homeId ", this.homeBook._id)
+    }
   }
   
-  address(cart: any){
+  
+  isAddedToCart(cart: any){
+    
     cart.displaybag = false;
 
-    for (this.cart1 of this.books){
-      let a = console.log("a", cart._id)
-      let b = console.log("b", this.cart1._id)
-      if (a==b){
-        cart.displaybag = true;
-      }
-    }
+    console.log("cart in home ",this.cartBooks)
+
+    // for (this.cart1 of this.books){
+    //   let a = console.log("a", cart._id)
+    //   let b = console.log("b", this.cart1._id)
+    //   if (a==b){
+    //     cart.displaybag = true;
+    //   }
+    // }
+
     let reqData = {
       "id": cart._id,
       "token": this.token
     }
-    this.service.addCart(reqData, this.token).subscribe((data) => {
-      console.log(data)
-    },
-    error => {
-      console.log(error);
-    });    
+    this.service.addCart(reqData, this.token).subscribe((response) => {
+      console.log(response)
+    },error => {
+        console.log(error);
+    } );    
   }
   
   whishlist(cart: any){
